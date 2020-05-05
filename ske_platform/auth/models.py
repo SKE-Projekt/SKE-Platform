cf4880de7b01878fd9c763b1c5027ad93bb22b12
Kamil Tokarski <openeduxyz@gmail.com>
2020-05-05 20:31:52 +0200

    Login and register logic

--- ske_platform/auth/models.py
+++ ske_platform/auth/models.py
@@ -3,7 +3,7 @@ from flask_login import UserMixin
 from ske_platform.database import db
 
 
-class User(UserMixin, db):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(128), unique=True, nullable=False)
     username = db.Column(db.String(128), unique=True, nullable=False)
--- ske_platform/auth/routes.py
+++ ske_platform/auth/routes.py
@@ -1,7 +1,11 @@
-from flask import render_template
+from flask import render_template, request, flash, redirect,abort
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, logout_user, login_required
 
+from ske_platform.database import db
 from . import auth, login_manager
 from .forms import LoginForm, RegisterForm
+from .models import User
 
 
 @login_manager.user_loader
@@ -11,12 +15,46 @@ def load_user(user_id):
         return User.query.get(user_id)
     return None
 
-@auth.route('/login')
+@login_manager.unauthorized_handler
+def unauthorized_callback():
+    return redirect('login')
+
+@auth.route('/login', methods=['POST', 'GET'])
 def login():
-    login_form = LoginForm()
+    login_form = LoginForm(request.form)
+
+    if request.method == 'POST' and login_form.validate():
+        user = User.query.filter_by(username=login_form.username.data).first()
+        if user is None:
+            flash('Nie ma takiego użytkownika!', 'danger')
+        else:
+            if check_password_hash(user.password, login_form.password.data):
+                login_user(user)
+            else:
+                flash('Złe hasło i/lub nazwa użytkownika!', 'danger')
     return render_template('auth/login.html', login_form=login_form)
 
-@auth.route('/register')
+@auth.route('/logout')
+@login_required
+def logout():
+    try:
+        logout_user()
+    except:
+        flash('Stało się coś złego, skontaktuj się z nami!', 'danger')
+    finally:
+        flash('Poprawnie wylogowano. Do zobaczenia!', 'success')
+    return redirect('login')
+
+@auth.route('/register', methods=['POST', 'GET'])
 def register():
-    register_form = RegisterForm()
+    register_form = RegisterForm(request.form)
+
+    if request.method == 'POST' and register_form.validate():
+        if User.query.filter_by(username=register_form.username.data).first() != None:
+            flash('Nazwa użytkownika jest już zajęta!', 'danger')
+        else:
+            user = User(password=generate_password_hash(register_form.password.data), username=register_form.username.data, email=register_form.email.data, first_name=register_form.first_name.data, last_name=register_form.last_name.data, is_superuser=False)
+            db.session.add(user)
+            db.session.commit()
+            flash('Możesz się teraz zalogować!', 'success')
     return render_template('auth/register.html', register_form=register_form)
