eaaf838d71a2d77a2f9523ba8cb4d3d7619c99fd
Kamil Tokarski <openeduxyz@gmail.com>
2020-05-05 18:32:05 +0200

    Database setup

--- ske_platform/__init__.py
+++ ske_platform/__init__.py
@@ -1,7 +1,15 @@
 import os
 
 from flask import Flask
+from flask_migrate import Migrate
 
 
 app = Flask(__name__)
 app.config.from_object(os.getenv('CONFIG_CLASS'))
+
+from .database import db
+db.init_app(app)
+mg = Migrate(app, db)
+
+from .auth import auth
+app.register_blueprint(auth)
--- ske_platform/conf/base_config.py
+++ ske_platform/conf/base_config.py
@@ -2,5 +2,8 @@ import os
 
 
 class BaseConfig:
-    BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+    BASE_DIR = os.path.abspath(os.path.dirname(__name__))
     SECRET_KEY = os.getenv('SECRET_KEY')
+
+    # Database settings
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
