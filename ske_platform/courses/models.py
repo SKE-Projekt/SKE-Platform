f6a140a993d58693fa4ec65588b1e3c29183f613
Kamil Tokarski <openeduxyz@gmail.com>
2020-05-11 03:45:21 +0200

    Intro to courses

--- ske_platform/courses/models.py
+++ ske_platform/courses/models.py
@@ -5,7 +5,7 @@ class Course(db.Model):
     id = db.Column(db.Integer, primary_key=True)
 
     id_ord = db.Column(db.Integer)
-    path = db.Column(db.String(128), unique=True)
+    path = db.Column(db.String, unique=True)
 
     # father_id of -1 represents
     # top-most course
@@ -13,3 +13,16 @@ class Course(db.Model):
 
     body_md = db.Column(db.String)
     body_html = db.Column(db.String)
+
+
+class Exercise(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    id_ord = db.Column(db.Integer)
+
+    path = db.Column(db.String, unique=True)
+    course_id = db.Column(db.Integer, nullable=True, default=-1)
+
+    body_md = db.Column(db.String)
+    body_html = db.Column(db.String)
+
+    start_code = db.Column(db.String)
--- ske_platform/courses/routes.py
+++ ske_platform/courses/routes.py
@@ -1,1 +1,9 @@
+from flask import render_template, redirect, request
+
+from . import courses
 from .models import Course
+
+
+@courses.route('/courses')
+def listCourses():
+    return render_template('courses/list_courses.html')
